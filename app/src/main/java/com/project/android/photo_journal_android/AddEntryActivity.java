d1469a42876c46dbcfec05f14e7e59ef8c531ed4
blubberbu <brianhandoko22@gmail.com>
2022-01-10 15:31:17 +0700

    Add back button

--- app/src/main/java/com/project/android/photo_journal_android/AddEntryActivity.java
+++ app/src/main/java/com/project/android/photo_journal_android/AddEntryActivity.java
@@ -4,10 +4,12 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -39,8 +41,7 @@ public class AddEntryActivity extends AppCompatActivity {
         btnBrowseImage = findViewById(R.id.btnBrowseImage);
         db = new DatabaseHelper(this);
 
-
-
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         btnBrowseImage.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -74,6 +75,8 @@ public class AddEntryActivity extends AppCompatActivity {
                 }
                 db.insertEntry(entryModel);
 //                getEntry();
+                Intent intent = new Intent(AddEntryActivity.this, MainActivity.class);
+                startActivity(intent);
             }
         });
     }
